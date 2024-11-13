a0f08aa07734e029edcb6785840c1cc8714634d4
lee kinanga <lee254kinanga@gmail.com>
2024-11-13 12:35:03 +0300

    leenx-weather api first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//HARD-CODED WEATHER DATA
+const weatherData = [
+    {
+        city: 'Nairobi',
+        temperature: 30.4,
+        himidity: 60,
+        wind_speed: 12.3,
+        description: 'Cloudy'
+    },
+    {
+        city: 'Mombasa',
+        temperature: 36.4,
+        himidity: 70,
+        wind_speed: 20.2,
+        description: 'Sunny'
+    },
+    {
+        city: 'Kisumu',
+        temperature: 28.4,
+        himidity: 50,
+        wind_speed: 15.3,
+        description: 'Rainy'
+    },
+    {
+        city: 'Nakuru',
+        temperature: 26.4,
+        himidity: 40,
+        wind_speed: 10.2,
+        description: 'Overcast'
+    },
+    {
+        city: 'Eldoret',
+        temperature: 32.4,
+        himidity: 60,
+        wind_speed: 18.3,
+        description: 'Cloudy'
+    },
+    {
+        city: 'Kisii',
+        temperature: 29.4,
+        himidity: 55,
+        wind_speed: 16.2,
+        description: 'Sunny'
+    },
+    {
+        city: 'Bungoma',
+        temperature: 29.4,
+        himidity: 55,
+        wind_speed: 16.2,
+        description: 'Sunny'
+    }
+];
+
+// Endpoint to get weather data for a specific city
+app.get('/leenx-weather', (req, res)=>{
+    const {city} = req.query;
+
+    if (city){
+        const cityWeather = weatherData.find((w) => w.city.toLowerCase() === city.toLowerCase());
+
+        if (cityWeather){
+            return res.json(cityWeather);
+        } else{
+            return res.status(404).json({error: 'City not found'});
+        }       
+        
+    }
+
+    res.json(weatherData);
+
+});
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`Weather api is running at http://localhost:${PORT}`);
+});
